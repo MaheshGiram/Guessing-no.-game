4c66d0cf37a1886e72aad1b63d112dd8aae85a73
mahesh Giram <girammahesh627@gmail.com>
2026-08-10 18:07:57 +0530

    add loop and conditinal statement

--- game.py
+++ game.py
@@ -4,4 +4,17 @@ def play_game():
     lucky_num = random.randint(1,50)
 
     #user num input
-    user_num = int(input("guess the lucky num: "))
+    while True:
+        user_num = int(input("guess the lucky num: "))
+
+        if user_num == lucky_num:
+            print("You won the Game!")
+            break
+        elif user_num < lucky_num:
+            print("num is to low")
+        else:
+            print("num is to high")
+
+    print("thank for play the game.")
+
+play_game()
